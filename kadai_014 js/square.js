0d8d9fb4ff39c38dced81459d4c5a322ef092eec
kamedayasuchika <kame.1230yk.chika@gmail.com>
2024-08-28 15:39:42 +0900

    課題修正

--- kadai_014 js/square.js
+++ kadai_014 js/square.js
@@ -1,10 +1,14 @@
 // 関数の作り方
 const squarecalc = (number) => {
-    console.log(Math.pow(number , 2));
+    return(Math.pow(number , 2));
   }
   
   // 関数の呼び出し方
   let number = Math.floor(Math.random() * 10) + 1;
   console.log(number);
-  squarecalc(number);
+  
+  console.log(squarecalc(number));
+  
+  
+
   
